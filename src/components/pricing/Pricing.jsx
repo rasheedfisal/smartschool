36ee6718790740c25cffab429de05fbf4d4d69a3
DELL <rasheed.fisal2015@gmail.com>
2023-08-19 18:02:51 +0200

    first commit3

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,21 +1,22 @@
-import React from "react"
-import Back from "../common/back/Back"
-import PriceCard from "./PriceCard"
-import "./price.css"
-import Faq from "./Faq"
+import React from "react";
+import Back from "../common/back/Back";
+import PriceCard from "./PriceCard";
+import "./price.css";
+import Faq from "./Faq";
+import AnimatedPage from "../common/AnimatedPage";
 
 const Pricing = () => {
   return (
-    <>
-      <Back title='Choose The Right Plan' />
-      <section className='price padding'>
-        <div className='container grid'>
+    <AnimatedPage>
+      <Back title="Choose The Right Plan" />
+      <section className="price padding">
+        <div className="container grid">
           <PriceCard />
         </div>
       </section>
       <Faq />
-    </>
-  )
-}
+    </AnimatedPage>
+  );
+};
 
-export default Pricing
+export default Pricing;
